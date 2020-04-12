b18887500ad24b7c40740072496fe338979d2537
naman653 <naman653@gmail.com>
2020-04-13 00:23:47 +0530

    added shop class

---
+++ app/src/main/java/com/hackathon/covid/data/Shop.kt
@@ -0,0 +1,10 @@
+package com.hackathon.covid.data
+
+class Shop (
+        var shopName: String = "",
+        var phone: String = "",
+        var masks: Int = 0,
+        var sanitizer: Int = 0,
+        var latitude: String = "",
+        var longitude: String = ""
+)
